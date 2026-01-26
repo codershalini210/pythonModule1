c332094ce3f76905ecbae95b7e9a63418dc5d3cc
shalini <s.rathore43@gmail.com>
2026-01-26 15:25:52 +0530

    list in python added

---
+++ listsinpython.py
@@ -0,0 +1,11 @@
+students= ["JOhn","Ron","Ranny"]
+marks = [25,65,32,52]
+a = ["Demo",121,41,"Hello"]
+print(students)
+print(marks)
+print(a)
+print("first student is ",students[0])
+print("second student is ",students[1])
+print("third student is ",students[2])
+# print("4th student is ",students[3]) 
+# above line will give error IndexError: list index out of range
