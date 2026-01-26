6ff54c417ab62352392c4b0832aa4dbf5a67c741
shalini <s.rathore43@gmail.com>
2026-01-26 15:53:36 +0530

    khkhjkhkj

--- listsinpython.py
+++ listsinpython.py
@@ -1,11 +1,24 @@
-students= ["JOhn","Ron","Ranny"]
-marks = [25,65,32,52]
-a = ["Demo",121,41,"Hello"]
-print(students)
-print(marks)
-print(a)
-print("first student is ",students[0])
-print("second student is ",students[1])
-print("third student is ",students[2])
+username = "Jose"
+print(username)
+username = "Raman"
+print(username)
+
+persons = ["john","Ron","Mary"]
+print("first person is ",persons[0])
+persons[0]="Johny"
+print("first person is ",persons[0])
+print(persons)
+persons=["a","b","c"]
+print(persons)
+
+# students= ["JOhn","Ron","Ranny"]
+# marks = [25,65,32,52]
+# a = ["Demo",121,41,"Hello"]
+# print(students)
+# print(marks)
+# print(a)
+# print("first student is ",students[0])
+# print("second student is ",students[1])
+# print("third student is ",students[2])
 # print("4th student is ",students[3]) 
 # above line will give error IndexError: list index out of range
