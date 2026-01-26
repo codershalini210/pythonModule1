13202dc9fb271ab52cf3b7dccfda6a131244382d
shalini <s.rathore43@gmail.com>
2026-01-26 16:58:32 +0530

    set and dictionary eg

---
+++ dictonaryeg.py
@@ -0,0 +1,6 @@
+student1 = {"name":"John",
+            "city":"Paris",
+            "age":22}
+print(student1["name"])
+student1["name"]="Ron"
+print(student1["name"])
---
+++ setseg.py
@@ -0,0 +1,5 @@
+set1 = {1,2,3,4,5}
+print(set1)
+list1 =["Apple","Banana","Orange","kiwi","Banana","Orange","Pineapple"]
+fruits_set = set(list1)
+print(fruits_set)
