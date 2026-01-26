9d2e7de3d65f5f43690540dcd7fdf14ed7771022
shalini <s.rathore43@gmail.com>
2026-01-26 16:38:38 +0530

    tupleegadded

---
+++ tupleeg.py
@@ -0,0 +1,12 @@
+t1 = (101,102,103)
+t2=("person1","Person2","person3")
+t3 =(101,"person1",102,"Person2",52.5)
+print(t1)
+print(t2)
+print(t3)
+stuple = ("John",)
+print(stuple)
+print(t1[0],t1[1],t1[2])
+# t1[2]=202
+# TypeError: 'tuple' object does not support item assignment
+# print(t1)
